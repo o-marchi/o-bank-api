37f1f796df0b03c5920a369cc99a96f95c35b6bf
Onelio de Marchi Junior <oneliojr@gmail.com>
2018-12-27 04:30:12 -0200

    User Changeset | Add and validate unique fields

--- lib/obank/accounts/user.ex
+++ lib/obank/accounts/user.ex
@@ -8,6 +8,7 @@ defmodule Obank.Accounts.User do
     field :email, :string
     field :name, :string
     field :username, :string
+    field :cpf, :string
 
     field :password, :string, virtual: true
     field :password_hash, :string
@@ -18,20 +19,23 @@ defmodule Obank.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :username, :email, :password])
-    |> validate_required([:name, :username, :email, :password])
-    |> validate_format(:username, ~r/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){3,38}/)
+    |> cast(attrs, [:name, :username, :email, :cpf, :password])
+    |> validate_required([:name, :username, :email, :cpf, :password])
+    # Username
+    |> unique_constraint(:username)
+    |> validate_format(:username, ~r/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){3,50}/)
+    # E-mail
+    |> unique_constraint(:email)
     |> validate_format(:email, ~r/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/)
+    # CPF
+    |> unique_constraint(:cpf)
+    |> validate_format(:cpf, ~r/[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/)
+    # Password
     |> put_password_hash
   end
 
-  defp put_password_hash(changeset) do
-    case changeset do
-      %Ecto.Changeset{valid?: true, changes: %{password: pass}}
-        ->
-          put_change(changeset, :password_hash, hashpwsalt(pass))
-      _ ->
-          changeset
-    end
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: pass}} = changeset) do
+    put_change(changeset, :password_hash, hashpwsalt(pass))
   end
+  defp put_password_hash(changeset) do changeset end
 end
