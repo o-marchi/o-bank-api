151dd1db3e90401f26595b2b4740c2fd3b298515
Onelio de Marchi Junior <oneliojr@gmail.com>
2018-12-30 00:40:06 -0200

    User | Add amount modifications on User model

--- lib/obank/accounts/user.ex
+++ lib/obank/accounts/user.ex
@@ -9,6 +9,7 @@ defmodule Obank.Accounts.User do
     field :name, :string
     field :username, :string
     field :cpf, :string
+    field :amount, :integer
 
     field :password, :string, virtual: true
     field :password_hash, :string
@@ -17,13 +18,18 @@ defmodule Obank.Accounts.User do
   end
 
   @doc false
+  def creation_changeset(user, attrs) do
+    changeset(user, attrs)
+    |> put_initial_amount
+  end
+
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:name, :username, :email, :cpf, :password])
     |> validate_required([:name, :username, :email, :cpf, :password])
     # Username
     |> unique_constraint(:username)
-    |> validate_format(:username, ~r/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){3,50}/)
+    |> validate_format(:username, ~r/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){2,51}/)
     # E-mail
     |> unique_constraint(:email)
     |> validate_format(:email, ~r/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/)
@@ -34,8 +40,15 @@ defmodule Obank.Accounts.User do
     |> put_password_hash
   end
 
+  @doc false
   defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: pass}} = changeset) do
     put_change(changeset, :password_hash, hashpwsalt(pass))
   end
   defp put_password_hash(changeset), do: changeset
+
+  @doc false
+  defp put_initial_amount(%Ecto.Changeset{valid?: true} = changeset) do
+    put_change(changeset, :amount, 100000)
+  end
+  defp put_initial_amount(changeset), do: changeset
 end
